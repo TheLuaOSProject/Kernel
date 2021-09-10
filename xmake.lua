901f8a0526d9b29c85e83843d11cc75f99a7ccf4
Frityet <ambhogal01@gmail.com>
2021-09-09 23:50:38 -0700

    Moved to xMake! Makefile deleted

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,7 @@
 add_rules("mode.debug", "mode.release");
 
+
+
 toolchain("cross-toolchain");
     set_kind("standalone");
     set_sdkdir("/usr/local/i386elfgcc/");
@@ -8,9 +10,12 @@ toolchain("cross-toolchain");
     set_toolset("c", "i386-elf-gcc");
     set_toolset("ld", "i386-elf-ld");
     set_toolset("as", "nasm");
+
+    
 toolchain_end();
 
 target("LuaOS-Boot");
+    set_plat("cross");
     set_kind("binary");
     set_toolchains("cross-toolchain");
 
@@ -20,9 +25,18 @@ target("LuaOS-Boot");
 
     set_objectdir("build/boot/");
     set_targetdir("build/boot/bin");
+
+    set_strip("none");
+
+    on_link(function (target) end);
+    after_link(function (target) 
+        os.rm(target:targetdir() .. "/boot.bin");
+        os.cp(target:objectdir() .. "/boot/boot.asm.o", target:targetdir() .. "/boot.bin", {rootdir = "build", symlink = false});
+    end);
 target_end();
 
 target("LuaOS-kernel");
+    set_plat("cross");
     set_kind("binary");
     set_toolchains("cross-toolchain");
 
@@ -32,7 +46,12 @@ target("LuaOS-kernel");
 
     add_asflags("-f elf32", { force = true });
     add_cflags("-ffreestanding", { force = true });
-    add_ldflags("-Ttext 0x1000", "--oformat binary");
+    add_ldflags("-Ttext 0x1000", { force = true });
     set_objectdir("build/kernel/");
     set_targetdir("build/kernel/bin");
+
+    after_link(function (target) 
+        os.mkdir("build/bin");
+        os.execv("cat", {"build/boot/bin/boot.bin", target:targetdir() .. "/LuaOS-kernel"}, {stdout = "build/bin/luaos.bin"});
+    end);
 target_end();
