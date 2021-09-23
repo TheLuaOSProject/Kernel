0ecaf5d16d7f619f48e99e6ec40f15a47e4b1617
Frityet <ambhogal01@gmail.com>
2021-09-23 12:51:41 -0700

    Dont need a mem map

--- kernel/memory/EFI_memory_map.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Frityet on 2021-09-23.
-//
-
-#include "EFI_memory_map.h"
--- kernel/memory/EFI_memory_map.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Frityet on 2021-09-23.
-//
-
-#ifndef LUAOS_EFI_MEMORY_MAP
-#define LUAOS_EFI_MEMORY_MAP
-
-#endif //LUAOS_EFI_MEMORY_MAP
