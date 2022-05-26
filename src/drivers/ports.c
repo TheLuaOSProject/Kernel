7fb33720f48a00035850411d2b48e41b81f6422d
Amrit Bhogal <ambhogal01@gmail.com>
2022-05-26 13:17:48 -0700

    Ports dont exist on AArch64

--- src/drivers/ports.c
+++
@@ -1,9 +0,0 @@
-///
-///@file ports.c
-///@author Amrit Bhogal
-///@version 1.0.0 - Amrit Bhogal on 2022-05-26
-///
-
-#include "ports.h"
-
-#include "common.h"
--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,9 @@
 #include "common.h"
 #include "types.h"
 
-#include "drivers/ports.h"
-
 
 ATTRIBUTE(noreturn, used)
 int kernel_start(struct stivale2_struct *bootloader)
 {
     (void)bootloader;
-    port.byte.out(124, 3);
 }
