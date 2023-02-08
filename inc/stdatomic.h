d96bde25f57a68ff76fccd241f40f0b1acb920af
pitust <piotr@stelmaszek.com>
2023-02-08 14:19:44 +0000

    chore: add some copyright headers to all the files

--- inc/stdatomic.h
+++ inc/stdatomic.h
@@ -1,6 +1,23 @@
-#pragma once
+/**
+ * Copyright (C) 2023 pitust
+ *
+ * This file is part of LuaOS.
+ *
+ * LuaOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LuaOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LuaOS.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-// Based on the LLVM header, licensed under Apache 2.0
+#pragma once
 
 #include <stddef.h>
 #include <stdint.h>
--- src/arch/x86_64/gdt.c
+++ src/arch/x86_64/gdt.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2023 Amrit Bhogal
+ * Copyright (C) 2023 Amrit Bhogal, pitust
  *
  * This file is part of LuaOS.
  *
