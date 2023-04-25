7e2c4a9bcd731c66fe8aa312e3485fb0d68b65ad
Louis Dupraz <louis.dupraz@epitech.eu>
2023-04-25 09:27:48 +0200

    Added testing for exp and fmod

--- src/lib/math.c
+++ src/lib/math.c
@@ -18,6 +18,9 @@
  */
 
 #define NAN 0.0 / 0.0
+# define HUGE_VAL 1e10000
+# define HUGE_VALF 1e10000f
+# define INF HUGE_VALF
 #include <float.h>
 #include <stdio.h>
 #include <math.h>
@@ -35,17 +38,34 @@ int cisinf(double x)
     return (x > DBL_MAX || x < -DBL_MAX);
 }
 
+double custom_abs(double x)
+{
+    if (x < 0)
+        return -x;
+    return x;
+}
+
 // Getting the remainder of x / y using euclidian division
 double cfmod(double x, double y)
 {
-    if (cisnan(x) || cisnan(y) || y == 0 || cisinf(x))
+    if (cisnan(x) || cisnan(y))
         return NAN;
+    if (y == 0 || cisinf(x)) {
+        errno = EDOM;
+        return NAN;
+    }
     if (x == 0)
         return 0;
-    double remainder = x;
-    while (remainder > y)
-        remainder -= y;
-    return remainder;
+    int signx = 1, signy = 1;
+    if (x < 0)
+        signx = -1;
+    if (y < 0)
+        signy = -1;
+    double remainder = custom_abs(x);
+    double absy = custom_abs(y);
+    while (remainder > absy)
+        remainder -= absy;
+    return remainder * signx;
 }
 
 // log() implementation using minimax polynomial approximations
@@ -70,7 +90,7 @@ double custom_log(double x) {
 // exp() implementation using Taylor series
 double custom_exp(double x) {
     double result = 1;
-    const int terms = 500000;
+    const int terms = 5000000;
     double current_term = 1;
     for (int i = 1; i <= terms; ++i) {
         current_term *= x / i;
@@ -90,17 +110,32 @@ double cuspow(double x, double y)
 
 int main(void)
 {
-
-    printf("-1 %f %f %s\n", exp(-1), custom_exp(-1), (exp(-1) == custom_exp(-1))
-    ? "ok" : "ko");
-    printf("0 %f %f %s\n", exp(0), custom_exp(0), (exp(0) == custom_exp(0))
+    printf("Fmod testing\n");
+    printf("NaN x : %f %f\n", fmod(NAN, 4), cfmod(NAN, 4));
+    printf("NaN y : %f %f\n", fmod(7, NAN), cfmod(7, NAN));
+    printf("y = 0 : %f %f\n", fmod(8, 0), cfmod(8, 0));
+    printf("Inf x : %f %f\n", fmod(INF, 4), cfmod(INF, 4));
+    printf("-2 / 4 : %f %f %s\n", fmod(-2, 4), cfmod(-2, 4), (fmod(-2, 4) ==
+    cfmod(-2, 4)) ? "ok" : "ko");
+    printf("-542.17 / -21.2 : %.15f %.15f %s\n", fmod(-542.17, -21.2), cfmod
+    (-542.17, -21.2), (fmod(-542.17, -21.2) == cfmod(-542.17, -21.2)) ? "ok"
+    : "ko");
+    printf("8 / 3 : %f %f %s\n", fmod(8, 3), cfmod(8, 3), (fmod(8, 3) ==
+    cfmod(8, 3)) ? "ok" : "ko");
+    printf("634.53 / 45.4: %.15f %.15f %s\n", fmod(634.53, 45.4), cfmod(634.53,
+        45.4), (fmod(634.53, 45.4) == cfmod(634.53, 45.40)) ? "ok" : "ko");
+    printf("\n\nExp testing\n");
+    printf("-1 %.20f %.20f %s\n\n", exp(-1), custom_exp(-1), (exp(-1) ==
+    custom_exp(-1)) ? "ok" : "ko");
+    printf("0 %f %f %s\n\n", exp(0), custom_exp(0), (exp(0) == custom_exp(0))
         ? "ok" : "ko");
-    printf("42 %f %f %s\n", exp(42), custom_exp(42), (exp(42) == custom_exp(42))
+    printf("42 %f %f %s\n\n", exp(42), custom_exp(42), (exp(42) == custom_exp
+    (42))
         ? "ok" : "ko");
-    printf("-2.3 %f %f %s\n", exp(-2.3), custom_exp(-2.3), (exp(-2.3) ==
+    printf("-2.3 %.20f %.20f %s\n\n", exp(-2.3), custom_exp(-2.3), (exp(-2.3) ==
     custom_exp(2.3)) ? "ok" : "ko");
-    printf("0.64 %f %f %s\n", exp(0.64), custom_exp(0.64), (exp(0.64) ==
+    printf("0.64 %.20f %.20f %s\n\n", exp(0.64), custom_exp(0.64), (exp(0.64) ==
     custom_exp(0.64)) ? "ok" : "ko");
-    printf("69.42 %f %f %s\n", exp(69.42), custom_exp(69.42), (exp(69.42) ==
+    printf("69.42 %f %f %s\n\n", exp(69.42), custom_exp(69.42), (exp(69.42) ==
     custom_exp(69.42)) ? "ok" : "ko");
 }
